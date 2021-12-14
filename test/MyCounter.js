5a00ffe936b5f81325bfca086c1069d8529378b1
Jorge del Casar <948953+jorgecasar@users.noreply.github.com>
2021-12-14 13:29:47 +0100

    move to vanilla

--- test/MyCounter.js
+++ test/MyCounter.js
@@ -1,55 +1,88 @@
-import { html, render } from "lit-html";
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    * {
+      font-size: 200%;
+    }
+
+    span {
+      width: 4rem;
+      display: inline-block;
+      text-align: center;
+    }
+
+    button {
+      width: 4rem;
+      height: 4rem;
+      border: none;
+      border-radius: 10px;
+      background-color: seagreen;
+      color: white;
+    }
+  </style>
+  <button id="dec">-</button>
+  <span id="count"></span>
+  <button id="inc">+</button>`;
 
 export class MyCounter extends HTMLElement {
+
+	static get observedAttributes() {
+		return ['count'];
+	}
+
   constructor() {
     super();
-    this.count = 0;
+		this.inc = this.inc.bind(this);
+    this.dec = this.dec.bind(this);
 
-    this.attachShadow({ mode: "open" });
+		this.attachShadow({ mode: "open" });
+		this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-    this.update();
+		this.incBtn = this.shadowRoot.getElementById('inc');
+		this.decBtn = this.shadowRoot.getElementById('dec');
+		this.countEl = this.shadowRoot.getElementById('count');
   }
 
+	connectedCallback() {
+    this.incBtn.addEventListener('click', this.inc);
+		this.incBtn.addEventListener('click', this.dec);
+		if (!this.hasAttribute('value')) {
+			this.setAttribute('value', 0);
+		}
+  }
+
+	disconnectedCallback() {
+		this.incBtn.removeEventListener('click', this.inc);
+		this.decBtn.removeEventListener('click', this.dec);
+	}
+
+	attributeChangedCallback() {
+		this.update();
+	}
+
+
+	get count() {
+		return Number(this.getAttribute('count')) || 0;
+	}
+
+	set count(value) {
+		this.setAttribute('count', value);
+		const event = new CustomEvent('count-changed', { detail: value });
+		this.dispatchEvent(event);
+	}
+
   inc() {
-    this.count++;
+    this.count += 1;
     this.update();
   }
 
   dec() {
-    this.count--;
+    this.count -= 1;
     this.update();
   }
 
-  template() {
-    return html`
-      <style>
-        * {
-          font-size: 200%;
-        }
-
-        span {
-          width: 4rem;
-          display: inline-block;
-          text-align: center;
-        }
-
-        button {
-          width: 64px;
-          height: 64px;
-          border: none;
-          border-radius: 10px;
-          background-color: seagreen;
-          color: white;
-        }
-      </style>
-      <button @click="${this.dec}">-</button>
-      <span>${this.count}</span>
-      <button @click="${this.inc}">+</button>
-    `;
-  }
-
   update() {
-    render(this.template(), this.shadowRoot, { eventContext: this });
+		this.countEl.textContent = this.count;
   }
 }
 
